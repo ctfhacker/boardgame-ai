ea6303818a0fa55ec153d9b4b73f2bd0ffaae83c
thebarbershopper <cld251@gmail.com>
2017-12-12 09:33:09 -0600

    Bad folder

--- boardgameai-rs/example-games/play-game/target_old/debug/deps/play_game-d2fa233e5c3d37f2.d
+++
@@ -1,5 +0,0 @@
-/home/ctf/workspace/boardgame-ai/boardgameai-rs/example-games/play-game/target/debug/deps/play_game-d2fa233e5c3d37f2: src/main.rs
-
-/home/ctf/workspace/boardgame-ai/boardgameai-rs/example-games/play-game/target/debug/deps/play_game-d2fa233e5c3d37f2.d: src/main.rs
-
-src/main.rs:
